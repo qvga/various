37e8976d10ab17f4839d8ee93f28aa22aec73735
Jakob Eriksson <jakob.eriksson@live.com>
2019-12-29 20:08:19 +0100

    sendtip

--- jormungandr/sendtiptopooltools.sh
+++ jormungandr/sendtiptopooltools.sh
@@ -2,6 +2,6 @@
 
 source .env
 
-TIP=$(./jcli rest v0 node stats get --output-format json -h http://127.0.0.1:${REST_PORT}/api | jq -r .lastBlockHeight)
+TIP=$(./jcli rest v0 node stats get --output-format json -h http://127.0.0.1:${REST_PORT}/api | /usr/bin/jq -r .lastBlockHeight)
 
 curl -G "https://api.pooltool.io/v0/sharemytip?poolid=${STAKE_POOL_ID}&userid=${PT_USER_ID}&genesispref=8e4d2a343f3dcf93&mytip=${TIP}"
