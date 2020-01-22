91b47563d022d25ca54c3dd590908dc4461cbfe3
Jakob Eriksson <jakob.eriksson@live.com>
2020-01-22 11:53:02 +0100

    send email when ip changes

---
+++ checkip/ip-alert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source .env
+
+newip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+oldip=$(cat ip.txt)
+
+echo "IP: ${newip}"
+
+if [[ "$newip" != "$oldip" ]]; then
+   echo "NEW IP: ${newip}"
+   echo "${oldip} -> ${newip}" | mail -s '[IP-ALERT] IP changed' -r "$MAILFROM" "$MAILTO"
+   echo "$newip" > ip.txt
+fi
