d11849fa0ecd7cf1320b7e39c59795f0e6e37600
jakob <jakob.eriksson@live.com>
2020-03-21 13:15:58 +0100

    add sqldump script

---
+++ prod-mysql/sqldump.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/snap/bin/docker exec mysql sh -c 'exec nice -n 20 mysqldump --triggers --routines --events --all-databases --add-drop-database -uroot -p"$MYSQL_ROOT_PASSWORD"' | gzip -9  > ~/sqldump.sql.gz
