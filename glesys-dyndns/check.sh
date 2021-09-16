7e06b8b9a9f48bc99e02ca99d7f47e36c9d52999
jakob <jakob.eriksson@live.com>
2021-09-16 20:16:04 +0200

    add dynamic dns script

---
+++ glesys-dyndns/check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source .env
+
+myip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+dnsip=$(dig +short "$HOST" @resolver1.opendns.com)
+
+echo "My IP: ${myip}  DNS IP: ${dnsip}"
+
+if [[ "$myip" != "$dnsip" ]]; then
+   echo "IP changed: ${newip}"
+   curl -X POST --basic -u "${API_KEY}" --data-urlencode "recordid=${RECORD_ID}" --data-urlencode "data=${myip}" https://api.glesys.com/domain/updaterecord
+   echo "My IP: ${myip}  DNS IP: ${dnsip}" | mail -s '[ALERT] IP changed' "$MAILTO"
+fi
--- jormungandr/sendtiptopooltools.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source .env
-
-TIP=$(./jcli rest v0 node stats get --output-format json -h http://127.0.0.1:${REST_PORT}/api | /usr/bin/jq -r .lastBlockHeight)
-
-curl -G "https://api.pooltool.io/v0/sharemytip?poolid=${STAKE_POOL_ID}&userid=${PT_USER_ID}&genesispref=8e4d2a343f3dcf93&mytip=${TIP}"
